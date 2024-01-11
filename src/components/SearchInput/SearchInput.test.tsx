fba9b1155003bd883b69873b1f18f30093fa0886
laureac <laure.a.claret@gmail.com>
2024-01-11 18:14:22 +0100

    fix: remove unecessary test

--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,25 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import SearchInput from "./SearchInput";
-import { debounce } from "../../utils/debounce";
-
-describe("debounceSearch", () => {
-  jest.useFakeTimers();
-
-  it("should only execute the function after the specified delay", () => {
-    const mockFunction = jest.fn();
-    const debouncedFunction = debounce(mockFunction, 500);
-
-    for (let i = 0; i < 10; i++) {
-      debouncedFunction();
-    }
-
-    expect(mockFunction).not.toHaveBeenCalled();
-
-    jest.runAllTimers();
-
-    expect(mockFunction).toHaveBeenCalledTimes(1);
-  });
-});
 
 test("handles rate limit error", async () => {
   global.fetch = jest
