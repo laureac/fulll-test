57782a446b39eaae10ac600ee8c5749501bcfb20
laureac <laure.a.claret@gmail.com>
2024-01-11 15:43:17 +0100

    feat: delete users and duplicate users components

--- src/components/DeleteItems/DeleteItems.tsx
+++ src/components/DeleteItems/DeleteItems.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Icon from "../Icon/Icon";
+import { UsersContext } from "../../context/UsersContext";
+import { GitHubUser } from "../../types/githubProfiles";
 
 const DeleteItems: React.FC = () => {
+  const context = useContext(UsersContext);
+
+  if (!context) {
+    throw new Error("must be used within a SelectedCardsProvider");
+  }
+
+  const { users, setUsers, selectedCards, setSelectedCards } = context;
+
+  const deleteSelectedUsers = () => {
+    const updatedUsers: GitHubUser[] = users.filter(
+      (user) => !selectedCards.includes(user.id)
+    );
+
+    setUsers(updatedUsers);
+    setSelectedCards([]);
+  };
   return (
-    <div>
+    <button onClick={deleteSelectedUsers} className="actions-btn">
       <Icon iconName="trash" />
-    </div>
+    </button>
   );
 };
 
--- src/components/DuplicateItems/DuplicateItems.tsx
+++ src/components/DuplicateItems/DuplicateItems.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Icon from "../Icon/Icon";
+import { UsersContext } from "../../context/UsersContext";
+import { GitHubUser } from "../../types/githubProfiles";
 
 const DuplicateItems: React.FC = () => {
+  const context = useContext(UsersContext);
+
+  if (!context) {
+    throw new Error("must be used within a SelectedCardsProvider");
+  }
+
+  const { users, setUsers, selectedCards } = context;
+
+  const duplicateSelectedUsers = () => {
+    const usersToDuplicate = users.filter((user) =>
+      selectedCards.includes(user.id)
+    );
+    const duplicateUsers: GitHubUser[] = [...usersToDuplicate];
+    setUsers([...users, ...duplicateUsers]);
+  };
+
   return (
-    <div>
+    <button onClick={duplicateSelectedUsers} className="actions-btn">
       <Icon iconName="copy" />
-    </div>
+    </button>
   );
 };
 
