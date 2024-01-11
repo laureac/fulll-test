b569977e967d8ae310fa2b57480915afa0103e5f
laureac <laure.a.claret@gmail.com>
2024-01-11 10:29:18 +0100

    feat: context

---
+++ src/context/UsersContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState } from "react";
+import { GitHubUser } from "../types/githubProfiles";
+
+type UsersContextType = {
+  users: GitHubUser[];
+  setUsers: (users: GitHubUser[]) => void;
+  selectedCards: number[];
+  setSelectedCards: (selectedCards: number[]) => void;
+  toggleCardSelection: (cardId: number) => void;
+};
+
+const defaultToggleCardSelection = (cardId: number) => {
+  console.warn("toggleCardSelection called without a provider", cardId);
+};
+
+export const UsersContext = createContext<UsersContextType>({
+  users: [],
+  setUsers: () => {},
+  selectedCards: [],
+  setSelectedCards: () => {},
+  toggleCardSelection: defaultToggleCardSelection,
+});
+
+type UsersProviderProps = {
+  children: React.ReactNode;
+};
+
+export const UsersProvider: React.FC<UsersProviderProps> = ({ children }) => {
+  const [selectedCards, setSelectedCards] = useState<number[]>([]);
+  const [users, setUsers] = useState<GitHubUser[]>([]);
+
+  const toggleCardSelection = (cardId: number) => {
+    setSelectedCards((prev) => {
+      // Check if the cardId is already in the array. If exist remove it, otherwise add it.
+      if (prev.includes(cardId)) {
+        return prev.filter((id) => id !== cardId);
+      } else {
+        return [...prev, cardId];
+      }
+    });
+  };
+
+  return (
+    <UsersContext.Provider
+      value={{
+        selectedCards,
+        toggleCardSelection,
+        users,
+        setUsers,
+        setSelectedCards,
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  );
+};
